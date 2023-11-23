e1865839717b3c6da51144e8afb312cad85e41ad
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-23 12:22:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
+let i = 11;
+let scroll = document.getElementById("infi-list");
+scroll.addEventListener("scroll", () => {
+  let trigger = scroll.scrollHeight - scroll.clientHeight; //scroll height--> total height of container and clientHeight -->total visible height
+  if (trigger - scroll.scrollTop <= 5) {
+    let ele = document.createElement("li");
+    ele.innerHTML = `Item ${i}`;
+    scroll.appendChild(ele);
+    i++;
+  }
+});
+for (let i = 1; i <= 10; i++) {
+  let ele = document.createElement("li");
+  ele.innerHTML = `Item ${i}`;
+  scroll.appendChild(ele);
+}
 
